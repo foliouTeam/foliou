0168ca0c2d70c48fc769d7d60bf60c74ef10d9b9
unknown <liupeng@ztgame.com>
2018-09-10 12:11:45 +0800

    同步代码，同步开发进度

--- build.js
+++ build.js
@@ -1,8 +1,14 @@
+//1、读取 组件下面的所有资源文件，css,js,html,tpl,shtml,sass,json,jpg,png,gif,svg
+//压缩图片
+//生成 assets/tmp.js文件,打包产生assets/index.js;
+
 var rollup = require('rollup');
 var json = require('rollup-plugin-json');
 var postcss = require('rollup-plugin-postcss');
 var postimage = require('@timdp/rollup-plugin-image');
 var posthtml = require('rollup-plugin-posthtml-template');
+
+
 async function build(input) {
     const bundle = await rollup.rollup({
         input: input,
