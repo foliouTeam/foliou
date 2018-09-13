f733067950bd011bce6b6ef258d648425824d647
foliou <foliou@outlook.com>
2018-09-13 17:10:36 +0800

    测试同步

--- build/files.js
+++ build/files.js
@@ -14,6 +14,7 @@ class Files{
                     if(err){
                         throw err;
                     }
+            
                     
                     if(st.isFile()){
                         readable=fs.createReadStream(_src);//创建读取流
