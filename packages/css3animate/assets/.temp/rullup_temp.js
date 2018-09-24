dcbf4be8562635a238c4f3993da13446263f7cdb
foliou <foliou@outlook.com>
2018-09-24 20:02:19 +0800

    删除多余的文件

--- packages/css3animate/assets/.temp/rullup_temp.js
+++
@@ -1,8 +0,0 @@
-var assets = {};
-import css/style_scss from "../css/style.scss";
-assets["css/style.scss"]=css/style_scss;
-import images/icon_png from "../images/icon.png";
-assets["images/icon.png"]=images/icon_png;
-import json/test_json from "../json/test.json";
-assets["json/test.json"]=json/test_json;
-export default assets;
