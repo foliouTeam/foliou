69ec6199517f59800838ec5e4b76154ca8a39a84
liupeng <liupeng@ztgame.com>
2019-11-20 15:03:39 +0800

    同步代码

--- demos/src/js/bgm.js
+++ demos/src/js/bgm.js
@@ -1,4 +1,4 @@
-var BGM = require("../../../packages/foliou/bgm/");
+var BGM = require("../../../packages/foliou/bgm");
 var $ = require("jquery");
 var bgm = new BGM({
 	file: "http://www.ztgame.com/act/30th/sound/bg.mp3",
